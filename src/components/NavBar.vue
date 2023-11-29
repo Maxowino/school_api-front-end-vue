ec4d88af5bd531cd9bf55aeff1f851d3128febcd
Maxowino <maxwellowino27@gmail.com>
2023-11-29 12:24:26 +0300

    view backend data

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,19 +1,21 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">School API</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+          <router-link to="/" class="nav-link">Home</router-link>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <!-- <a class="nav-link" href="#">Courses</a> -->
+          <router-link to="/courses" class="nav-link">Courses</router-link>
         </li>
-        <li class="nav-item dropdown">
+        <!-- <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Dropdown
           </a>
@@ -26,7 +28,7 @@
         </li>
         <li class="nav-item">
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
+        </li> -->
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
--- src/views/CoursesPage.vue
+++ src/views/CoursesPage.vue
@@ -1,31 +1,46 @@
 <template>
+   <div class="container">
     <table class="table table-dark table-striped">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Amount</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
+    <tr v-for="course in courses" :key="course.id">
+      <th scope="row">{{course.id}}</th>
+      <td>{{course.name}}</td>
+      <td>{{course.amount}}</td>
+      <td>
+        <button class="btn btn-warning">Update</button>
+        <button class="btn btn-danger">Delete</button>
+      </td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
+   
   </tbody>
-</table>
+</table> 
+   </div>
 </template>
+<script>
+import axios from 'axios';
+   export default{
+       name:'CoursesPage',
+       data(){
+            return{
+                courses:[],
+            }
+       },
+       methods:{
+        async getCourses(){
+            const response = await axios.get("http://127.0.0.1:8000/api/get-courses");
+            this.courses= response.data;
+        }
+       },
+       created(){
+        this.getCourses();
+       }
+   }
+</script>
